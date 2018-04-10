e71845857a5b9fadcd0194b0e75fae82303c18e8
BuildTools <fernando.kga@gmail.com>
2018-04-10 19:34:18 -0300

    Correções no Projeto NetBeans.

--- (temp) COO/TelaCadastroHorario.java
+++ (temp) COO/TelaCadastroHorario.java
@@ -8,12 +8,12 @@
  *
  * @author a9788030
  */
-public class TelaCadastraHorario extends javax.swing.JFrame {
+public class TelaCadastroHorario extends javax.swing.JFrame {
 
     /**
      * Creates new form Tela
      */
-    public TelaCadastraHorario() {
+    public TelaCadastroHorario() {
         initComponents();
     }
 
@@ -271,20 +271,20 @@ public class TelaCadastraHorario extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaCadastraUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaCadastraUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaCadastraUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaCadastraUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroUsuario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaCadastraUsuario().setVisible(true);
+                new TelaCadastroUsuario().setVisible(true);
             }
         });
     }
