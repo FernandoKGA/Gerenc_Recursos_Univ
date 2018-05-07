a787b9b3e8ef313ec27be51349dab566528bcf74
Lucas-Perez <lucaschabariberi@usp.br>
2018-05-07 19:22:08 -0300

    Atualização das telas para o NetBeans

--- Projeto NetBeans/Projeto_COO/src/Telas/Telas.java
+++ Projeto NetBeans/Projeto_COO/src/Telas/Telas.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Telas;
 
 import java.awt.Component;
@@ -2027,4 +2022,5 @@ public class Telas extends JFrame {
     private JTextField textField;
     private JLabel label_4;
     private JTextField textField_1;
+    // End of variables declaration//GEN-END:variables
 }
