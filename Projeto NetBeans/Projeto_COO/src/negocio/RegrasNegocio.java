321fb3c24b4935eed4e4c4bd0b54f9f50ec3b938
Denise Adati <denisekfa@gmail.com>
2018-05-07 16:32:26 -0300

    coloquei função no botão cadastrar usuario

--- Projeto NetBeans/Projeto_COO/src/negocio/RegrasNegocio.java
+++ Projeto NetBeans/Projeto_COO/src/negocio/RegrasNegocio.java
@@ -6,6 +6,7 @@
 package negocio;
 
 import bancodados.*;
+import java.util.List;
 import objetos.*;
 
 /**
@@ -25,6 +26,24 @@ public class RegrasNegocio extends RegrasNegocioException {
         }
     }
 
+    public List<Usuario> listaUsuarios() throws RegrasNegocioException {
+        try {
+            return baseDados.listaUsuarios();
+        } catch (Banco_de_DadosException e) {
+            e.printStackTrace();
+            throw new RegrasNegocioException("Não foi possível"
+                    + " conectar ao Banco de Dados.");
+        }
+    }
+
+    public boolean verificaCoordenador(String curso, String cargo) throws RegrasNegocioException {
+            List<Usuario> a = listaUsuarios();
+            for (Usuario user : a) {
+                if (user.getCargo().equals(cargo) && user.getCurso().equals(curso))return false;
+            }
+            return true;
+    }
+
     public void cadastraUsuario(String nome, String nUSP,
             String email, String telefone, String curso,
             String cargo) throws RegrasNegocioException {
@@ -38,40 +57,39 @@ public class RegrasNegocio extends RegrasNegocioException {
                     + " conectar ao Banco de Dados.");
         }
     }
-    
+
     public void cadastraRecurso(String nome, String tipo, String predio)
-    throws RegrasNegocioException{
+            throws RegrasNegocioException {
         Recurso rs = new Recurso();
         rs.setNome(nome);
         rs.setTipo(tipo);
         rs.setPredio(predio);
-        
-        try{
+
+        try {
             baseDados.insereRecurso(rs);
-        }catch(Banco_de_DadosException e){
+        } catch (Banco_de_DadosException e) {
             e.printStackTrace();
             throw new RegrasNegocioException("Não foi possível "
                     + "conectar ao Banco de Dados.");
         }
     }
-    
-    public void cadastraReserva(int horaInicio, int horaFim, String data, Usuario usuario, Recurso recurso) throws RegrasNegocioException{
-        
-        try{
+
+    public void cadastraReserva(int horaInicio, int horaFim, String data, Usuario usuario, Recurso recurso) throws RegrasNegocioException {
+
+        try {
             Reserva r = new Reserva();
             r.setData(data);
             r.setHoraInicio(horaInicio);
             r.setHoraFim(horaFim);
             r.setUsuario(usuario);
             r.setRecurso(recurso);
-            
+
             baseDados.insereReserva(r);
-        }catch(Banco_de_DadosException e){
+        } catch (Banco_de_DadosException e) {
             e.printStackTrace();
             throw new RegrasNegocioException("Não foi possível conectar "
                     + "ao banco de dados.");
         }
     }
-    
-    
+
 }
