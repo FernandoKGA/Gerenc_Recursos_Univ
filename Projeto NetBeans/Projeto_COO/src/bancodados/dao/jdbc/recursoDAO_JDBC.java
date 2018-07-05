f9c5a64e077945cc089226c568bfffb1b760d3cf
Lucas <lucas.pcbranco@hotmail.com>
2018-07-05 19:24:02 -0300

    Corrigido problemas - criacaoBancoDados 100% FUNCIONAL!

--- Projeto NetBeans/Projeto_COO/src/bancodados/dao/jdbc/recursoDAO_JDBC.java
+++ Projeto NetBeans/Projeto_COO/src/bancodados/dao/jdbc/recursoDAO_JDBC.java
@@ -81,6 +81,7 @@ public class recursoDAO_JDBC extends ConectorDAO_JDBC implements recursoDAO{
                 r.setPredio(predio);
                 r.setTipo(tipo);
                 r.setId_Recurso(idrecurso);
+                System.out.println("BUSCA RECURSO_DAOJDBC: " + r.getId_Recurso());
             }
         } catch (SQLException e) {
             Log.gravaLog(e);
